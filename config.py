7bf137bf2230e48e58821b609ea4bae679fd01f6
ritikroy960 <132186112+ritikroy960@users.noreply.github.com>
2023-11-14 13:11:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
       API_HASH = getenv("API_HASH", "")
       API_ID = int(getenv("API_ID", ""))
       BOT_TOKEN = getenv("BOT_TOKEN", "")
-      AS_COPY = True if getenv("AS_COPY", True) == "`{defult}`" else massage.forward
+      AS_COPY = True if getenv("AS_COPY", True) == "`{defult}`" else message.forward
       CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
       
 
