dda9e63eb5100fdcbd4358b3e04bc02c3549069b
ritikroy960 <132186112+ritikroy960@users.noreply.github.com>
2023-11-14 12:28:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
       API_HASH = getenv("API_HASH", "")
       API_ID = int(getenv("API_ID", ""))
       BOT_TOKEN = getenv("BOT_TOKEN", "")
-      AS_COPY = True if getenv("AS_COPY", True) == "`{defult}`" else True
-      CHANNEL = tuple(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      AS_COPY = True if getenv("AS_COPY", True) == "`{defult}`" in True
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
       
 
 # Don't Remove Credit @VJ_Botz
